332f4122d127130c32aeb76b9068cfa232479610
KrishKannaa <kannaa.krishnaa@gmail.com>
2019-09-15 14:21:16 +0530

    Created basic code for saving code as XLSX and PDF
    Created basic code for saving code as XLSX and PDF

---
+++ EPPLUS-SaveAsXlsxOrPdf/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace EPPLUS_SaveAsXlsxOrPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Save_btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Save_btn
+            // 
+            this.Save_btn.Location = new System.Drawing.Point(206, 158);
+            this.Save_btn.Name = "Save_btn";
+            this.Save_btn.Size = new System.Drawing.Size(75, 44);
+            this.Save_btn.TabIndex = 0;
+            this.Save_btn.Text = "Save";
+            this.Save_btn.UseVisualStyleBackColor = true;
+            this.Save_btn.Click += new System.EventHandler(this.Save_btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 262);
+            this.Controls.Add(this.Save_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Save_btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ EPPLUS-SaveAsXlsxOrPdf/Form1.cs
@@ -0,0 +1,53 @@
+﻿using OfficeOpenXml;
+using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EPPLUS_SaveAsXlsxOrPdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_btn_Click(object sender, EventArgs e)
+        {
+            SaveAsPdf("C:\\Users\\kannan\\Downloads\\test.pdf");
+        }
+
+        private bool SaveAsPdf(string saveAsLocation)
+        {
+            string saveas = (saveAsLocation.Split('.')[0]) + ".pdf";
+            try
+            {
+                Workbook workbook = new Workbook();
+                workbook.LoadFromFile(saveAsLocation);
+
+                //Save the document in PDF format
+
+                workbook.SaveToFile(saveas, Spire.Xls.FileFormat.PDF);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+        //public static MemoryStream ConvertXLSXtoPDF(MemoryStream stream)
+        //{
+          
+        //}
+            
+    }
+}
